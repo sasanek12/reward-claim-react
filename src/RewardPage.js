6ca5112550b1e82c638f08a033465e316afdaab0
sasanek12 <sasanek12@gmail.com>
2025-03-07 17:59:08 +0100

    Naprawiono wyświetlanie obrazków w CAPTCHA

--- src/RewardPage.js
+++ src/RewardPage.js
@@ -3,6 +3,7 @@ import rewardImage from "./assets/images/reward.png";
 export default function RewardPage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-green-500 to-green-800 text-white text-center p-6">
+            <h1 className="text-4xl font-extrabold mb-4">Enjoy your twink <3</h1>
             <img src={rewardImage} alt="Nagroda" className="w-full max-w-2xl rounded-lg shadow-xl" />
             <h2 className="text-2xl font-bold mt-6">Ale to nie wszystko!</h2>
             <p className="text-lg mt-2">Informacje wkrótce!</p>
